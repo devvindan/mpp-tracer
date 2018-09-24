ae167e6bdb131f3b056bea006d7cf7c4ddc523a8
devvindan <devvindan@gmail.com>
2018-09-24 23:14:11 +0300

    Added serialization

---
+++ Tracer/JsonSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Tracer
+{
+    // Сериализация в JSON 
+    class JsonSerializer : ISerializer
+    {
+
+        private DataContractJsonSerializer serializer;
+
+        public void Serialize(TraceResult result, Stream stream)
+        {
+            using (XmlDictionaryWriter writer = JsonReaderWriterFactory.CreateJsonWriter(stream, Encoding.UTF8, ownsStream: true, indent: true))
+            {
+                serializer.WriteObject(writer, result);
+            }
+        }
+
+        public JsonSerializer()
+        {
+            serializer = new DataContractJsonSerializer(typeof(TraceResult));
+        }
+    }
+}
--- Tracer/XmlSerializer.cs
+++ Tracer/XmlSerializer.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
+using System.Runtime.Serialization;
 
 namespace Tracer
 {
     class XmlSerializer : ISerializer
     {
+        // Настройки 
+        private XmlWriterSettings settings;
+
+        protected readonly DataContractSerializer xmlSerializer;
+
         public void Serialize(TraceResult result, Stream stream)
         {
+            using (XmlWriter writer = XmlWriter.Create(stream, settings))
+            {
+                xmlSerializer.WriteObject(writer, result);
+            }
 
+        }
+
+        public XmlSerializer()
+        {
+            // Настройки отступов
+            settings = new XmlWriterSettings { Indent = true };
+            xmlSerializer = new DataContractSerializer(typeof(TraceResult));
         }
     }
 }
